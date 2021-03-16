799858ec03954a9cb3a8c8fd2948adf5f656faf2
youme <mrsimplelife@naver.com>
2021-03-17 03:17:39 +0900

    훅으로

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,29 +1,51 @@
-import { Component } from "react";
 import "../app.css";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
-class SimpleHabit extends Component {
-  state = {
-    count: 0,
-  };
+const SimpleHabit = (props) => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+  useEffect(() => {
+    console.log("mounted");
+  }, []);
+  return (
+    <li className="habit">
+      <span ref={spanRef}>Reading</span>
+      <span className="habit__count">{count}</span>
+      <button
+        className="habit__button habit__button--increase"
+        onClick={handleIncrement}
+      >
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
+// class SimpleHabit extends Component {
+//   state = {
+//     count: 0,
+//   };
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+//   handleIncrement = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
 
-  render() {
-    return (
-      <li className="habit">
-        <span>Reading</span>
-        <span className="habit__count">{this.state.count}</span>
-        <button
-          className="habit__button habit__button--increase"
-          onClick={this.handleIncrement}
-        >
-          <i className="fas fa-plus-square"></i>
-        </button>
-      </li>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <li className="habit">
+//         <span>Reading</span>
+//         <span className="habit__count">{this.state.count}</span>
+//         <button
+//           className="habit__button habit__button--increase"
+//           onClick={this.handleIncrement}
+//         >
+//           <i className="fas fa-plus-square"></i>
+//         </button>
+//       </li>
+//     );
+//   }
+// }
 
 export default SimpleHabit;
